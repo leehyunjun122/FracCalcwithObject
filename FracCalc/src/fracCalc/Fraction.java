049dfbde2d6a767e44295343fa321d68b2a272a5
Alex Lee <Alex Lee@DESKTOP-VSMC0R8>
2019-01-23 21:48:49 -0800

    OLE!

--- FracCalc/src/fracCalc/Fraction.java
+++ FracCalc/src/fracCalc/Fraction.java
@@ -1,7 +1,126 @@
 package fracCalc;
 
 public class Fraction {
-	private String operand1;
-	private String operator;
-	private String operand2;
+	private int numerator = 0;
+	private int denominator = 1;//zero cannot be the denominator
+	private int whole = 0;
+	
+	public Fraction(String fraction) {
+		parseFrac(fraction);
+	}
+	public Fraction(int whole, int numerator, int denominator) {
+		this.whole = whole;
+		this.numerator = numerator;
+		this.denominator = denominator;
+	}
+	private void parseFrac(String operand) {
+		if (operand.contains("/") && operand.contains("_")) {
+			numerator = (Integer.parseInt(operand.split("_")[1].split("/")[0]));
+			denominator = (Integer.parseInt(operand.split("/")[1]));
+			whole = (Integer.parseInt(operand.split("_")[0]));
+		} else if (operand.contains("/") && !(operand.contains("_"))) {
+			numerator = (Integer.parseInt(operand.split("/")[0]));
+			denominator = (Integer.parseInt(operand.split("/")[1]));
+		} else {
+			whole = Integer.parseInt(operand);
+		}
+	}
+	public Fraction calculation(Fraction Second, String operator) {
+		this.toImproperFrac();
+		Second.toImproperFrac();
+		Fraction answer;
+		if (operator.contains("+")) {
+			answer = addition(Second);
+		} else {
+			if (operator.contains("-")) {
+				answer = subtraction(Second);
+			} else {
+				if (operator.contains("*")) {
+					answer = multiplication(Second);
+				} else {
+					if (operator.contains("/")) {
+						answer = division(Second);
+					} else {
+						throw new IllegalArgumentException ("Not a correct operator");
+					}
+				}
+			}
+		}
+		answer.reduce();
+		return answer;
+	}
+	private Fraction addition(Fraction Second) {
+		int num = this.numerator * Second.denominator + Second.numerator*this.denominator;
+		int denom = this.denominator * Second.denominator;
+		Fraction answer = new Fraction(0, num, denom);
+		return answer;
+	}
+	private Fraction subtraction(Fraction Second) {
+		int num = this.numerator * Second.denominator - Second.numerator*this.denominator;
+		int denom = this.denominator * Second.denominator;
+		Fraction answer = new Fraction(0, num, denom);
+		return answer;
+	}
+	private Fraction multiplication(Fraction Second) {
+		int num = this.numerator * Second.numerator;
+		int denom = this.denominator * Second.denominator;
+		Fraction answer = new Fraction(0, num, denom);
+		return answer;
+	}
+	private Fraction division(Fraction Second) {
+		int num = this.numerator * Second.denominator;
+		int denom = this.denominator * Second.numerator;
+		Fraction answer = new Fraction(0, num, denom);
+		return answer;
+	}
+	private void toImproperFrac() {
+		if (this.whole>=0) {
+			this.numerator = whole * denominator + numerator;
+		} else {
+			this.numerator = whole * denominator - numerator;
+		}
+		this.whole = 0;
+	}
+	private void reduce() {
+		if (denominator<0) {
+			denominator *= -1;
+			numerator *= -1;
+		}
+		whole = numerator/denominator;
+		numerator = numerator%denominator;
+		if (numerator<0 && whole!=0) {
+			numerator *= -1;
+		}
+		int gcf = gcf(numerator, denominator);
+		numerator /= gcf;
+		denominator /= gcf;
+    }
+	public String toString() {
+		if (this.numerator==0) {
+			return ""+ whole;
+		} 
+		if (this.whole==0) {
+			return "" + numerator + "/" + denominator;
+		}
+		return "" + whole +"_" + numerator + "/" + denominator;
+	}
+  //Calculates the greatest common factor of two integers
+    public static int gcf(int numerator, int denominator) {
+    	int gcf = 1; // Set the initial value of gcf to 1, which is the lowest possible gcf
+		int count; // Set it to whichever number is larger.
+    	if (numerator==denominator || numerator>denominator ) {
+    		count = denominator;
+    	} else {
+    		count = numerator;
+    	}
+    	if (count<0) {
+    		count *= -1;
+    	}
+		for(int i = 2; i <= count; i++) {
+			if(numerator%i == 0 && denominator%i == 0) {
+				gcf = i;
+			}
+		}
+		return gcf;
+    }
 }
